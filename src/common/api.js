1b679c07c347fbf5634299d9989c9a9fef297294
lvlinyuan <lly@lvlinyuandeMacBook-Air.local>
2019-01-19 19:15:02 +0800

    change

--- src/common/api.js
+++ src/common/api.js
@@ -2,19 +2,59 @@ import wepy from 'wepy'
 import m_contacts from '../mocks/contact';
 import m_history from '../mocks/history';
 import m_reply from '../mocks/reply';
+import global from './global';
 
 
 export default {
 	getMessageList () {
 		let history = m_history;
-		return new Promise( (resolve,reject) => {
+		// return new Promise( (resolve,reject) => {
 			let distince = [],
 				rst = [],
 				sorted = history.sort( (a,b) => b.time - a.time);
-				console.log(sorted);
-				resolve(sorted);
-		})
-	}
+				sorted.forEach(v => {
+					if (v.from !== 'me' && distince.indexOf(v.from) === -1) {
+						distince.push(v.from);
+					}
+					if (v.to !== 'me' && distince.indexOf(v.to) === -1) {
+						distince.push(v.to);
+					}
+				});
+				
+				distince.forEach((v) => {
+					let pmsg = sorted.filter((msg) => msg.to === v || msg.from === v);
+					let  lastmsg = pmsg.length ? pmsg[0].msg : '';
+
+					rst.push({
+						id: v,
+						lastmsg: lastmsg,
+					})
+				})
+				console.log(rst);
+				// setTimeout(() => {
+					return this.leftJoin(rst, m_contacts);
+				// });
+		// })
+	},
+	leftJoin(original, contacts) {
+		let contactObj = global.getContact();
+		console.log(contactObj);
+        let rst = [];
+
+        original.forEach((v) => {
+            if (!v.id) {
+                v.id = (v.from !== 'me') ? v.from : v.to;
+            }
+            if (v.id) {
+                if (v.id !== 'me') {
+                    v.name = contactObj[v.id].name;
+                    v.icon = (wepy.env === 'web' ? './mocks/users/' : '../mocks/users/') + contactObj[v.id].id + '.png';
+                }
+                rst.push(v);
+            }
+		});
+        return rst;
+    },
 }
 
  
---
+++ src/mocks/history.js
@@ -0,0 +1,8 @@
+export default [
+    {'to': 'songjiangwei', 'from': 'me', 'type': 'text', 'msg': '错没错', 'time': 1480338091374},
+    {'to': 'me', 'from': 'songjiangwei', 'type': 'text', 'msg': '我错了，你说啥都对！', 'time': 1480338091375},
+    {'to': 'tanshuoshuo', 'from': 'me', 'type': 'text', 'msg': '为什么你们家的猪那么贵。', 'time': 1480338091378},
+    {'to': 'me', 'from': 'tanshuoshuo', 'type': 'text', 'msg': '因为他会飞。', 'time': 1480338091379},
+    {'to': 'xiaoxiaolv', 'from': 'me', 'type': 'text', 'msg': 'hi', 'time': 1480338091384},
+    {'to': 'me', 'from': 'xiaoxiaolv', 'type': 'text', 'msg': '哈啊哈', 'time': 1480338091394},
+];
